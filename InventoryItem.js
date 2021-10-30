019e00673aff1b39f58a17ff3e8a3a4281128a85
tenesiss <30935834+goku1080@users.noreply.github.com>
2021-10-30 20:52:31 -0300

    Last Commit fix

--- InventoryItem.js
+++ InventoryItem.js
@@ -1,3 +1,4 @@
+const Item = require('./Item.js');
 class InventoryItem {
     constructor(rest, raw, inventory){
         this.rest = rest;
@@ -10,7 +11,8 @@ class InventoryItem {
     async fetchItem(){ // Get Original Item
          var item = await this.rest.restObj.itemManager.get(this.itemID);
          if(!item) return false;
-         return item;
+         var iL = new Item(this.rest, item);
+         return iL;
     }
     add(amount){ // Adds an amount of the item to the inventory
      var inv = await this.rest.db.inventories.get(this.inventory.userID);
